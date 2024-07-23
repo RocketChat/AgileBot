28fabed3b0103e5cf8dbd953773c0a220d6d3ab2
hardik <hardikbhatia777@gmail.com>
2024-07-23 13:38:13 +0530

    chore: added percentages for detailed stats

--- lib/processors/QuickPollProcessor.ts
+++ lib/processors/QuickPollProcessor.ts
@@ -6,34 +6,39 @@ import { IPollData, Poll } from '../../definitions/PollProps';
 import { t } from '../../i18n/translation';
 
 export class QuickPollProcessor implements IProcessor {
-	public id = Poll.ProcessorId;
+    public id = Poll.ProcessorId;
 
-	public async processor(jobContext: IJobContext, read: IRead, modify: IModify, http: IHttp, persis: IPersistence) {
-		const { uuid } = jobContext;
+    public async processor(jobContext: IJobContext, read: IRead, modify: IModify, http: IHttp, persis: IPersistence) {
+        const { uuid } = jobContext;
 
-		const assoc = new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, uuid);
-		const [pollData] = (await read.getPersistenceReader().readByAssociation(assoc)) as IPollData[];
-		if (!pollData) {
-			console.error(t('poll_with_uuid_not_found'));
-			return;
-		}
+        const assoc = new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, uuid);
+        const [pollData] = (await read.getPersistenceReader().readByAssociation(assoc)) as IPollData[];
+        if (!pollData) {
+            console.error(t('poll_with_uuid_not_found'));
+            return;
+        }
 
-		const room = await read.getRoomReader().getById(pollData.roomId);
-		if (!room) {
-			console.error(t('poll_with_room_not_found'));
-			return;
-		}
+        const room = await read.getRoomReader().getById(pollData.roomId);
+        if (!room) {
+            console.error(t('poll_with_room_not_found'));
+            return;
+        }
 
-		let summaryResult = '';
-		if (pollData.responses.yes.length > pollData.responses.no.length) {
-			summaryResult = 'YES';
-		} else if (pollData.responses.no.length > pollData.responses.yes.length) {
-			summaryResult = 'NO';
-		} else {
-			summaryResult = 'TIE';
-		}
+        const totalVotes = pollData.responses.yes.length + pollData.responses.no.length;
 
-		const channelSummaryText = `
+        const yesPercentage = totalVotes > 0 ? (pollData.responses.yes.length / totalVotes) * 100 : 0;
+        const noPercentage = totalVotes > 0 ? (pollData.responses.no.length / totalVotes) * 100 : 0;
+
+        let summaryResult = '';
+        if (pollData.responses.yes.length > pollData.responses.no.length) {
+            summaryResult = 'YES';
+        } else if (pollData.responses.no.length > pollData.responses.yes.length) {
+            summaryResult = 'NO';
+        } else {
+            summaryResult = 'TIE';
+        }
+
+        const channelSummaryText = `
 ## Poll Results:
 ${pollData.pollMessage}
 Created by: ${pollData.creatorName}
@@ -41,29 +46,29 @@ Created by: ${pollData.creatorName}
 ## Verdict: ${summaryResult}
         `;
 
-		const detailedStatsText = `
+        const detailedStatsText = `
 ## Detailed Poll Results:
 ${pollData.pollMessage}
 
-Yes: ${pollData.responses.yes.join(', ')}
-No: ${pollData.responses.no.join(', ')}
+Yes (${yesPercentage.toFixed(2)}%): ${pollData.responses.yes.join(', ')}
+No (${noPercentage.toFixed(2)}%): ${pollData.responses.no.join(', ')}
         `;
 
-		const sender = await read.getUserReader().getAppUser();
-		if (!sender) {
-			console.error(t('poll_app_user_not_found'));
-			return;
-		}
+        const sender = await read.getUserReader().getAppUser();
+        if (!sender) {
+            console.error(t('poll_app_user_not_found'));
+            return;
+        }
 
-		await sendMessage(modify, room, sender, channelSummaryText);
+        await sendMessage(modify, room, sender, channelSummaryText);
 
-		const creator = await read.getUserReader().getById(pollData.creatorId);
-		if (creator) {
-			await sendDirectMessage(read, modify, creator, detailedStatsText, persis);
-		} else {
-			console.error(t('poll_creator_with_username_not_found'));
-		}
+        const creator = await read.getUserReader().getById(pollData.creatorId);
+        if (creator) {
+            await sendDirectMessage(read, modify, creator, detailedStatsText, persis);
+        } else {
+            console.error(t('poll_creator_with_username_not_found'));
+        }
 
-		await persis.removeByAssociation(assoc);
-	}
+        await persis.removeByAssociation(assoc);
+    }
 }
